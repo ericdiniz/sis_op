187af98d0bf4131b07f5674e6c0a9143406c220c
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-04-11 22:15:27 -0300

    implementações reunião 11-04

--- src/main/java/br/so/app/App.java
+++ src/main/java/br/so/app/App.java
@@ -1,7 +1,9 @@
 package br.so.app;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import br.so.entity.Cliente;
 import br.so.entity.Pedido;
 import br.so.importador.ImportCSV;
 
@@ -13,8 +15,48 @@ public class App
 {
     public static void main( String[] args )
     {
-      List<Pedido> pedidos = ImportCSV.readArqCSV("teste.txt");
 
-      pedidos.stream().sorted().forEach(p -> System.out.println(p.toString())); 
+      List<Pedido> pedidos = ImportCSV.readArqCSV("teste.txt");
+      List<Cliente> clientes = new ArrayList<Cliente>();
+
+      pedidos.sort(Pedido::compareTo);
+
+      System.out.println("Opção 1 - Prioridade de prazo de entrega e quantidade de produtos");
+
+      double tempoTotal = 0.0;
+    
+      for(Pedido p : pedidos) {
+
+        System.out.println(p.toString());
+
+        tempoTotal += p.getTempo();
+        System.out.println("Tempo total da estreira (segundos): " +  tempoTotal);
+        System.out.println("");
+
+      }
+
+      System.out.println("Média por pedido (segundos): " + (tempoTotal / pedidos.size()));
+
+      System.out.println("Fim Opção 1");
+
+      System.out.println("Opção 2 - Prioridade de prazo de entrega por cliente");
+
+      
+
+
+
+
+      System.out.println("Fim Opção 2");
+
+      System.out.println("Opção 3 - Duplicar Esteira: ");
+
+
+
+
+
+
+
+
+
     }
 }
--- src/main/java/br/so/entity/Cliente.java
+++ src/main/java/br/so/entity/Cliente.java
@@ -1,9 +1,15 @@
 package br.so.entity;
 
+import java.util.List;
+
 public class Cliente {
     
     private String nome;
+    private List<Pedido> pedidos;
     
+    
+    public Cliente() {
+    }
     /**
      * @param nome
      */
@@ -22,6 +28,19 @@ public class Cliente {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    
+
+
+
+
 
     
 }
