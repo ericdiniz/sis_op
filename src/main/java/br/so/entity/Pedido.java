65a2b999cd801b8ac2805907c40adecfee7e5ebf
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-04-11 22:15:34 -0300

    implementação

--- src/main/java/br/so/entity/Pedido.java
+++ src/main/java/br/so/entity/Pedido.java
@@ -1,10 +1,18 @@
 package br.so.entity;
 
+import java.util.Comparator;
+
 public class Pedido implements Comparable<Pedido> {
 
     private Cliente cliente;
     private int quantProdutos;
     private int prazoEmpc;
+    private static final double VOLUME_PROD = 250.0; // Volume da unidade de produto 
+    private static final double VOLUME_MAX_PAC = 5000.0; // Volume máximo de um pacote
+    private static final double TEMPO_FIX = 5; // 5 segundos 
+    private static final double TEMPO_TRANS_PROD = 0.5;
+
+    
 
     /**
      * @return the quantProdutos
@@ -12,30 +20,59 @@ public class Pedido implements Comparable<Pedido> {
     public int getQuantProdutos() {
         return quantProdutos;
     }
+
     /**
      * @param quantProdutos the quantProdutos to set
      */
     public void setQuantProdutos(int quantProdutos) {
         this.quantProdutos = quantProdutos;
     }
+
     /**
      * @return the prazoEmpc
      */
     public int getPrazoEmpc() {
+
+        if(prazoEmpc == 0)
+            return Integer.MAX_VALUE;
+
         return prazoEmpc;
     }
+
     /**
      * @param prazoEmpc the prazoEmpc to set
      */
     public void setPrazoEmpc(int prazoEmpc) {
         this.prazoEmpc = prazoEmpc;
     }
+
+    public double getTempo() {
+        return (this.getQuantPac() * TEMPO_FIX) + (this.getQuantProdutos() * TEMPO_TRANS_PROD);
+    }
+
     /**
      * @return the cliente
      */
     public Cliente getCliente() {
         return cliente;
     }
+
+    /**
+     * 
+     * @return Retorna a quantidade de pacotes que serão gerados nesse pedido. 
+     */
+    public double getQuantPac(){
+        return Math.ceil(getVolPed() / VOLUME_MAX_PAC);
+    }
+
+    /**
+     * 
+     * @return Retorna o volume total dos produtos do pedido em questão. 
+     */
+    public double getVolPed(){
+        return this.getQuantProdutos() * VOLUME_PROD;
+    }
+
     /**
      * @param cliente the cliente to set
      */
@@ -43,37 +80,34 @@ public class Pedido implements Comparable<Pedido> {
         this.cliente = cliente;
     }
 
-
-    
     @Override
     public String toString() {
-    
-        return "Cliente: " + this.cliente.getNome() + " | Quantidade de produtos: " + this.quantProdutos + " | Tempo de empacotamento: " + this.getPrazoEmpc();
+
+        return "Cliente: " + this.cliente.getNome() + " | Quantidade de produtos: " + this.quantProdutos
+                + " | Tempo de empacotamento: " 
+                + (this.getPrazoEmpc() == Integer.MAX_VALUE ? "Sem Prazo" : this.getPrazoEmpc())  
+                +  " | Volume Total Pedido: "  + this.getVolPed() 
+                + " | Número de pacotes: " + this.getQuantPac() + " | Tempo de empacotamento: " + this.getTempo();
     }
 
     @Override
     public int compareTo(Pedido o) {
 
-        if (this.prazoEmpc < o.prazoEmpc) {
-            return -1;
-        }
-        if (this.prazoEmpc > o.prazoEmpc) {
-            return 1;
-        }
-        return 0;
-    
+        return Comparator
+        .comparing(Pedido::getPrazoEmpc)
+        .thenComparing(Pedido::getQuantProdutos)
+        .compare(this, o);
+
     }
 
     // montar a esteira
     /*
-        Regras: 
-        - limite de volume
-        - prazo empacotamento 
-        - tempo fixo 5 seg
-        - tempo diario de 8h (4h manha e 4h tarde)
-        
-    */
+     * Regras:
+     * - limite de volume
+     * - prazo empacotamento
+     * - tempo fixo 5 seg
+     * - tempo diario de 8h (4h manha e 4h tarde)
+     * 
+     */
 
-
-    
 }
